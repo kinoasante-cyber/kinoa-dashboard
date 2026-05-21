a7ae009d4d32cba4c3fed0279850730c8169118e
Steven <kinoa.sante@gmail.com>
2026-05-21 09:04:22 -0400

    add vercel serverless proxy for GAS

---
+++ api/gas.js
@@ -0,0 +1,8 @@
+﻿export default async function handler(req, res) {
+  const GAS_URL = process.env.VITE_GAS_URL;
+  const params = new URLSearchParams(req.query);
+  const upstream = await fetch(`${GAS_URL}?${params}`, { redirect: 'follow' });
+  const body = await upstream.text();
+  res.setHeader('Content-Type', 'application/json');
+  res.status(upstream.status).send(body);
+}
